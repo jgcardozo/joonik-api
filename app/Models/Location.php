28490a801e16557fe2d91f2e37b5bfb0e77b004d
Juan Guillermo <jperez@enigmadev.com>
2024-10-22 15:37:46 -0500

    some tweaks

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Location extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = ["code", "name", "image", "created_at"];
+
+    protected $hidden = ['updated_at','deleted_at'];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('d-m-Y');
+    }
 }
